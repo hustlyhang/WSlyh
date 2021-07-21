53deadd3226beff71d9ebb2c951a50eaffe0974d
hustlyhang <1959983722@qq.com>
2021-07-21 20:27:23 +0800

    用小根堆实现定时器，还没测，明天来测试

---
+++ http/timer.cpp
@@ -0,0 +1,89 @@
+#include "timer.h"
+#include <string.h>
+
+HeapTimer::HeapTimer(int delaytime) {
+    this->expire_time = time(NULL) + delaytime;
+}
+
+
+TimerHeap::TimerHeap(int _capacity):m_capacity(_capacity), m_cur_num(0) {
+    m_timer_array = new HeapTimer*[m_capacity](); 
+    if (!m_timer_array) throw std::exception();
+}
+
+TimerHeap::~TimerHeap() {
+    for (int i = 0; i < m_cur_num; ++i) m_timer_array[i] = nullptr;
+    delete [] m_timer_array;
+}
+
+
+void TimerHeap::heap_down(int heap_node) {
+    HeapTimer* tmp_timer = m_timer_array[heap_node];
+    int child = 0;
+    for (; heap_node * 2 + 1 < m_cur_num; heap_node = child) {
+        child = heap_node * 2 + 1;
+        if (child + 1 < m_cur_num && m_timer_array[child + 1]->expire_time < m_timer_array[child]->expire_time) child ++;
+        if (tmp_timer->expire_time > m_timer_array[child]->expire_time) {
+            m_timer_array[heap_node] = m_timer_array[child];
+        }
+        else break;
+    }
+    m_timer_array[heap_node] = tmp_timer;
+}
+
+void TimerHeap::add_timer(HeapTimer* add_timer) {
+    if (!add_timer) return;
+    if (m_cur_num >= m_capacity) resize();
+    
+    int last_node = m_cur_num++;
+    int parent = 0;
+
+    // 新加入的节点在最后，向上调整
+    for( ; last_node > 0; last_node = parent ) {
+        parent = ( last_node - 1 ) / 2;
+        if( m_timer_array[parent]->expire_time > add_timer->expire_time ) {
+            m_timer_array[last_node] = m_timer_array[parent];
+        } else {
+            break;
+        }
+    }
+    m_timer_array[last_node] = add_timer;
+}
+
+void TimerHeap::del_timer( HeapTimer* del_timer ) {
+    if( !del_timer ) return;
+    
+    del_timer->callback_func = nullptr;
+}
+
+void TimerHeap::pop_timer() {
+    if( !m_cur_num ) return;
+    if( m_timer_array[0] ) {
+        m_timer_array[0] = m_timer_array[m_cur_num];
+        delete m_timer_array[m_cur_num--];
+        heap_down(0);  // 对新的根节点进行下滤
+    }
+}
+
+void TimerHeap::tick() {
+    HeapTimer* tmp_timer = m_timer_array[0];
+    time_t cur_time = time(NULL);
+    while(!m_cur_num) {
+        if(!tmp_timer) break;
+        if(tmp_timer->expire_time > cur_time) break;
+        if(m_timer_array[0]->callback_func) m_timer_array[0]->callback_func(m_timer_array[0]->m_client_data);
+        pop_timer();
+        tmp_timer = m_timer_array[0];
+    }
+}
+
+void TimerHeap::resize() {
+    HeapTimer** tmp = new HeapTimer*[m_capacity * 2]();
+    if(!tmp) throw std::exception();
+    memcpy(tmp, m_timer_array, m_cur_num);
+    m_capacity *= 2;
+    delete[] m_timer_array;
+    m_timer_array = tmp;
+}
+
+
--- http/timer.h
+++ http/timer.h
@@ -3,25 +3,57 @@
 #include "http.h"
 #include <vector>
 #include <sys/time.h>
+#include <netinet/in.h>
 #include <queue>
 #include <unistd.h>
 #include <iostream>
 #include <vector>
 
-// 每个timer都包含过期时间以及相绑定的用户数据
-struct timer
-{
-    timer(){};
-    struct timeval expire_time;
-    http* user_data;
-};
-struct cmp {
-    bool operator () (timer a, timer b) {
-        return a.expire_time.tv_sec > b.expire_time.tv_sec;
-    }
+class HeapTimer;
+
+struct client_data {
+    sockaddr_in address;        // 连接的地址
+    int sock_fd;                // http连接的文件描述符
+    HeapTimer* timer;           // 小根堆时间节点
 };
 
-std::priority_queue<timer*, std::vector<timer*>, cmp > top_timer
+class HeapTimer {
+public:
+    HeapTimer(int delaytime);
+    // 需要一个回调函数
+    void (*callback_func)(client_data*);
+public:
+    time_t expire_time;   // 定时器过期时间
+    client_data* m_client_data;
+};
+
+
+// 定时器小根堆
+class TimerHeap{
+public:
+    TimerHeap(int _capacity);
+    ~TimerHeap();
+
+public:
+    void heap_down(int heap_node);// 向下调整
+    void add_timer(HeapTimer*);
+    void del_timer(HeapTimer*);
+    void pop_timer();
+    void tick();
+
+    void resize(); // 当数组容纳不下的时候扩容
+
+
+private:
+    int m_capacity;
+    int m_cur_num;
+    HeapTimer** m_timer_array;
+};
+
+
+
+
+
 
 
 #endif
